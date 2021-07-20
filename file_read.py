0dc8ff439fee4841e2cb66bbf70f26bbbeb1daa0
Anton Spirin <lightyear1987@gmail.com>
2021-07-20 22:49:01 +0300

    some changes read_file

--- file_read.py
+++ file_read.py
@@ -1,3 +1,19 @@
-f = open('myfile.txt')
-print(f.read())
-f.close()
+'''f = open(r'myfile.txt')
+#print(f.read())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+f.close()'''
+
+
+'''with open(r'myfile.txt') as f:
+    for line in f:
+        print(line)'''
+
+
+f = open(r'myfile.txt')
+try:
+    for line in f:
+        print(line)
+finally:
+    f.close()
